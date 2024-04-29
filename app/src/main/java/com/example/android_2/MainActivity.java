ae2158a021de01d262980ee1157bb3e6f29a0a08
laurariistaniemi <162997772+laurariistaniemi@users.noreply.github.com>
2024-04-29 21:14:39 +0300

    again basics of a project, user puts name and birthmonth in and gets a text as a result. mayday11

--- app/src/main/java/com/example/android_2/MainActivity.java
+++ app/src/main/java/com/example/android_2/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity  {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //tähän vielä tekstiä
+
             }
         });
     }
