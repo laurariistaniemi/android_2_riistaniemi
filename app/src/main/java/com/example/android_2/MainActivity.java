35bed204bb8d7260cb1e4782eecc643496aa0e6a
laurariistaniemi <162997772+laurariistaniemi@users.noreply.github.com>
2024-04-30 11:03:55 +0300

    needs only minor adjustments anymore, it works but its clunky

--- app/src/main/java/com/example/android_2/MainActivity.java
+++ app/src/main/java/com/example/android_2/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
 
-
+            //haluan vain commit
 
         });
 
