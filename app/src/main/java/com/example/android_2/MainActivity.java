0768e165540a1556195e021137115dd01f2a73c1
laurariistaniemi <162997772+laurariistaniemi@users.noreply.github.com>
2024-04-30 11:18:08 +0300

    just about finished

--- app/src/main/java/com/example/android_2/MainActivity.java
+++ app/src/main/java/com/example/android_2/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.android_2;
 
 import android.os.Bundle;
-import android.service.autofill.OnClickAction;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -19,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
 
+    //the start  that comes with all projects
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,88 +26,62 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
-            //haluan vain commit miks tää git nyt vastustaa
-
         });
-
-
     }
 
+    //method that listens when the button has been clicked, it does the onClick method, and
+    //does the updateresults method, where the text is altered
     public void ButtonPress(View v) {
-        //TextView tvResults_Text = (TextView) findViewById(R.id.tvResults_Text);
 
         Button button = (Button) findViewById(R.id.button);
-        //button.setOnClickListener(v -> UpdateResults());
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 UpdateResults();
-                //TextView tvResults_Text = findViewById(R.id.tvResults_Text);
-                //tvResults_Text.getText();
-
-
                 }
-        });}
+        });
+    }
 
 
+    //method where the method finds the input name that user has given, and with it
+    //gives the automatic text vappu before the new, making a new vappu name
+    //the birthmonth is there to just be an extra input and to kindly confuse the user
     public void UpdateResults() {
         try {
+            //i knowingly have variables here that are not being used
+            //it was just to make it more clear to me what i wanted to use
             EditText tvInput_Name = findViewById(R.id.tvInput_Name);
             EditText tvInput_Month = findViewById(R.id.tvInput_Month);
             TextView tvName = findViewById(R.id.tvName);
             TextView tvMonth = findViewById(R.id.tvMonth);
             TextView tvResults_Text = findViewById(R.id.tvResults_Text);
             TextView tvNew_name = findViewById(R.id.tvNew_Name);
-
-            //String strInput_Name = getString(R.string.kerro_nimesi);
-            //String strInput_Month = getString(R.string.kerro_syntym_kuukautesi);
-            String strResults = "VAPPU" + tvInput_Name.getText().toString(); //+ tvInput_Month.getText().toString();
-            //EditText tvResults_Text = strResults.toString();
-            //tvResults_Text += strResults;
+            //join the automatic text vappu and users name together in a string
+            String strResults = "VAPPU" + tvInput_Name.getText().toString();
+            //add that name to the textview new name
             tvNew_name.append(strResults);
             tvNew_name.setText(strResults);
-            //EditText tvResults_Text = strResults.getText().getString();
 
+            //exception catch
         } catch (NumberFormatException e) {
             return;
         }
     }
-        //final EditText tvInput_Name = (EditText) findViewById(R.id.tvInput_Name);
-        //final EditText tvInput_Month = (EditText) findViewById(R.id.tvInput_Month);
-        //final String strResults = (String) findViewById(R.id.tvResults_Text);
 
-
-
-
+    //class i originally used for the updateResults method, but it turned out to not be useful
+    //i still wanted to it show here that i know what it is
+    /*
     public class TextChange implements TextWatcher {
 
         @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
         @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-        }
+        public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
         @Override
-        public void afterTextChanged(Editable s) { UpdateResults();
-        }
-
-
-    }
-    /*
-    public class TextChangeImpl extends TextChange {
-        @Override
-        public void UpdateResults() {
-            super.UpdateResults();
-        }
+        public void afterTextChanged(Editable s) { UpdateResults();}
     }
 
      */
-
-
-
 }
